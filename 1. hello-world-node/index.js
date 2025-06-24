f4f5f6529e18b9d1d9b2ffc239489e99831a93b0
sagrvma <notsagarverma@gmail.com>
2025-06-24 07:12:09 +0530

    how to run basic nodejs program

---
+++ 1. hello-world-node/index.js
@@ -0,0 +1,10 @@
+console.log("Hello World");
+
+const array = [1, 2, 3, 4];
+console.log(array, "array");
+
+setTimeout(() => {
+  console.log("This message is delayed by 2 seconds.");
+}, 2000);
+
+console.log("This is the last line of synchronous code.");
