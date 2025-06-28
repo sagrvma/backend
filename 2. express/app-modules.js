24892d755ed0cc3418f926aa9de2d2f6a4ef1396
sagrvma <notsagarverma@gmail.com>
2025-06-28 18:26:46 +0530

    hello world using express

---
+++ 2. express/app-modules.js
@@ -0,0 +1,31 @@
+const express = require("express");
+
+const app = express();
+
+//Application level settings
+app.set("view engine", "ejs");
+//Configures EJS(Embedded Javascript) as my template engine.
+//Means we can render dynamic HTML pages instead of just sending static responses.
+
+//Routing
+app.get("/", (req, res) => {
+  res.send("Home Page");
+});
+
+app.post("/api/data", (req, res) => {
+  res.json({
+    message: "Data recieved",
+    data: req.body,
+  });
+});
+
+//Error handling middleware
+app.use((err, req, res, next) => {
+  console.log(err.stack);
+  res.status(500).send("Something went wrong");
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is now running at ${port}`);
+});
---
+++ 2. express/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+
+const app = express(); //Creates the express application instance
+
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello world using Express JS!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at port ${port}`);
+});
+
+/* 
+RAW NODEJS EQUIVALENT OF THE ABOVE
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  if (req.method === "GET" && req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("Hello world using Express JS!");
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("Not Found");
+  }
+});
+
+server.listen(3000, () => {
+  console.log("Server is running at port 3000");
+});
+
+*/
