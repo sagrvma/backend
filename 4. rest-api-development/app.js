66957e48cb05c82c7e196d733f157480b88eb660
sagrvma <notsagarverma@gmail.com>
2025-06-29 11:33:08 +0530

    get all books, get a single book, add a book

--- 4. rest-api-development/app.js
+++ 4. rest-api-development/app.js
@@ -7,12 +7,12 @@ app.use(express.json());
 
 let books = [
   {
-    id: "1",
+    id: 1,
     title: "Book 1",
   },
   {
-    id: "1",
-    title: "Book 1",
+    id: 2,
+    title: "Book 2",
   },
 ];
 
@@ -26,6 +26,31 @@ app.get("/", (req, res) => {
 //Get all routes
 app.get("/get", (req, res) => {
   res.json(books);
+});
+
+//Get a single book
+app.get("/get/:id", (req, res) => {
+  const book = books.find((book) => book.id === req.params.id);
+  if (book) {
+    res.status(200).json(book);
+  } else {
+    res.status(404).json({
+      message: "Book not found!",
+    });
+  }
+});
+
+//Add a book
+app.post("/add", (req, res) => {
+  const newBook = {
+    id: books.length + 1,
+    title: `Book ${books.length + 1}`,
+  };
+  books.push(newBook);
+  res.status(200).json({
+    data: newBook,
+    message: "New book added successfully!",
+  });
 });
 
 const port = 3000;
