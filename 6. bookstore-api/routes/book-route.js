72960b0c967c48eedee65e2d0a33112a464c1a57
sagrvma <notsagarverma@gmail.com>
2025-07-01 21:44:27 +0530

    added bookRoutes router, boilerplate for crud operations for books and made the server use the bookRoute

---
+++ 6. bookstore-api/routes/book-route.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const {
+  getAllBooks,
+  getSingleBookById,
+  addBook,
+  updateBook,
+  deleteBook,
+} = require("../controllers/book-controller");
+
+//Create Express router
+const router = express.Router();
+
+//All routers that are related to books only
+router.get("/get", getAllBooks);
+router.get("/get/:id", getSingleBookById);
+router.post("/add", addBook);
+router.put("/update/:id", updateBook);
+router.delete("/delete/:id", deleteBook);
