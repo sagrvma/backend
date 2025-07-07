2c64804dead945f0c88ea26d006a59503a006322
sagrvma <notsagarverma@gmail.com>
2025-07-07 22:06:23 +0530

    Added logic for the register controller using bcrypt to store hashed password

--- 8. nodejs-auth/controllers/auth-controller.js
+++ 8. nodejs-auth/controllers/auth-controller.js
@@ -1,8 +1,50 @@
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+
 const registerUser = async (req, res) => {
   try {
+    //Extract user information
+    const { username, email, password, role } = req.body;
+
+    //Check if user already exists
+    const checkIfExists = await User.findOne({
+      $or: [{ username }, { email }],
+    });
+    if (checkIfExists) {
+      return res.status(400).json({
+        success: false,
+        message:
+          "User already exists with the same username/email! Try again with a different username/email.",
+      });
+    }
+    //Hash user password - Using bcrypt
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    //Create a new user and save in the database
+    const newlyCreatedUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+      role: role || "user",
+    });
+
+    await newlyCreatedUser.save();
+
+    if (newlyCreatedUser) {
+      return res.status(201).json({
+        success: true,
+        message: "User created successfully.",
+      });
+    } else {
+      return res.status(400).json({
+        success: false,
+        message: "Unable to create user! Please try again.",
+      });
+    }
   } catch (error) {
     console.log(error);
-    req.status(500).json({
+    return res.status(500).json({
       success: false,
       message: "Something went wrong!",
     });
@@ -13,7 +55,7 @@ const loginUser = async (req, res) => {
   try {
   } catch (error) {
     console.log(error);
-    req.status(500).json({
+    return res.status(500).json({
       success: false,
       message: "Something went wrong!",
     });
--- 8. nodejs-auth/server.js
+++ 8. nodejs-auth/server.js
@@ -1,11 +1,16 @@
 require("dotenv").config();
 const express = require("express");
 const connectToDB = require("./database/db");
+const authRouter = require("./routes/auth-routes");
 const app = express();
 
 const PORT = process.env.PORT;
 
 connectToDB();
+
+app.use(express.json()); //Middleware
+
+app.use("/auth", authRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is now successfully running on port ${PORT}`);
