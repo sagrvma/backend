749cacd88037fd09e009060d51e1313a6c37b743
sagrvma <notsagarverma@gmail.com>
2025-07-09 07:34:15 +0530

    added cloudinary upload helper function

---
+++ 8. nodejs-auth/helper/cloudinary-helper.js
@@ -0,0 +1,20 @@
+const cloudinary = require("../config/cloudinary");
+
+const uploadToCloudinary = async (filePath) => {
+  try {
+    const result = await cloudinary.uploader.upload(filePath);
+
+    return {
+      //Also returning these so later in case we need to update or delete the image, we can do that not only in our database but actually from clousinary as well
+      url: result.secure_url,
+      publicId: result.public_id,
+    };
+  } catch (error) {
+    console.error("Error while uploading image to Cloudinary!", error);
+    throw new Error("Error while uploading image to Cloudinary! ");
+  }
+};
+
+module.exports = {
+  uploadToCloudinary,
+};
