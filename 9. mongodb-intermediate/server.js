0f485fe478f7784a8424099cf42526c60bfedf40
sagrvma <notsagarverma@gmail.com>
2025-07-19 22:01:14 +0530

    added product routes

--- 9. mongodb-intermediate/server.js
+++ 9. mongodb-intermediate/server.js
@@ -1,13 +1,18 @@
 require("dotenv").config();
 const express = require("express");
 const connectToDB = require("./database/db");
+const productRouter = require("./routes/product-routes");
 
 const app = express();
 const port = process.env.PORT;
 
+//Connect to mongoDB
 connectToDB();
 
+//Middleware
 app.use(express.json());
+
+app.use("/products", productRouter);
 
 app.listen(port, () => {
   console.log(`App is successfully running on port ${port}`);
