56e95f91d9b8ea11864a46f395a71981f825fd78
sagrvma <notsagarverma@gmail.com>
2025-07-20 12:41:37 +0530

    added controller logic for fetching all products and filtered using aggregation pipeline

--- 9. mongodb-intermediate/controllers/product-controller.js
+++ 9. mongodb-intermediate/controllers/product-controller.js
@@ -1,5 +1,64 @@
 const Products = require("../models/products");
 
+const getAllProducts = async (req, res) => {
+  try {
+    const allProducts = await Products.find({});
+
+    if (allProducts.length == 0) {
+      return res.status(400).json({
+        success: false,
+        message: "No products found!",
+      });
+    }
+
+    return res.status(201).json({
+      status: true,
+      message: `Found ${allProducts.length} products.`,
+      products: allProducts,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: true,
+      message: "Something went wrong! Please try again",
+    });
+  }
+};
+
+const getFilteredProducts = async (req, res) => {
+  try {
+    const results = await Products.aggregate([
+      {
+        $match: {
+          inStock: true,
+          price: {
+            $gte: 100,
+          },
+        },
+      },
+    ]);
+
+    if (results.length == 0) {
+      return res.status(400).json({
+        success: false,
+        message: "No products found with the applied filters!",
+      });
+    }
+
+    return res.status(201).json({
+      success: true,
+      message: `Found ${results.length} products with the applied filters.`,
+      filteredProducts: results,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: "Something went wrong! Please try again.",
+    });
+  }
+};
+
 const insertSampleProducts = async (req, res) => {
   try {
     const sampleProducts = req.body;
@@ -19,4 +78,4 @@ const insertSampleProducts = async (req, res) => {
   }
 };
 
-module.exports = { insertSampleProducts };
+module.exports = { getAllProducts, getFilteredProducts, insertSampleProducts };
--- 9. mongodb-intermediate/routes/product-routes.js
+++ 9. mongodb-intermediate/routes/product-routes.js
@@ -1,8 +1,14 @@
 const express = require("express");
-const { insertSampleProducts } = require("../controllers/product-controller");
+const {
+  insertSampleProducts,
+  getAllProducts,
+  getFilteredProducts,
+} = require("../controllers/product-controller");
 
 const router = express.Router();
 
+router.get("/get", getAllProducts);
+router.get("/get-filter", getFilteredProducts);
 router.post("/add", insertSampleProducts);
 
 module.exports = router;
