4e8c59fdafe522c151e7b3a6509340a440c2d73f
sagrvma <notsagarverma@gmail.com>
2025-07-24 22:19:32 +0530

    defined the schema for the product, queries and the mutations

---
+++ 12. mongodb-graphql/src/graphql/schema.js
@@ -0,0 +1,37 @@
+const { gql } = require("graphql-tag");
+
+const typeDefs = gql`
+  type Product {
+    id: ID!
+    title: String!
+    category: String!
+    price: Float!
+    inStock: Boolean!
+  }
+
+  type Query {
+    product: [Product!]!
+    products(id: ID!): Product
+  }
+
+  type Mutation {
+    createProduct(
+      title: String!
+      category: String!
+      price: Float!
+      inStock: Boolean!
+    ): Product
+
+    deleteProduct(id: ID!): Boolean
+
+    updateProduct(
+      id: ID!
+      title: String
+      category: String
+      price: Float
+      inStock: Boolean
+    ): Product
+  }
+`;
+
+module.exports = typeDefs;
